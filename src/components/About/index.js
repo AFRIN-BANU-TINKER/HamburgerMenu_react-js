9cb82815b35f5a5cb576961e6746bfcedaedee85
AFRIN-BANU-TINKER <afrinappu26@gmail.com>
2023-05-11 14:15:00 +0000

    solution

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const About = () => (
+  <div className="about-page-card">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      className="sm-about-img"
+      alt="about"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      className="md-about-img"
+      alt="about"
+    />
+  </div>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-page-card">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      className="sm-home-img"
+      alt="home"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      className="md-home-img"
+      alt="home"
+    />
+  </div>
+)
+
+export default Home
